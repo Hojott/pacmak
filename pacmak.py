56632a41b69499b51793e2abdbc13734e631786c
Sakari Marttinen <sakari.marttinen@paivola.fi>
2022-11-07 13:40:22 +0200

    Finish script

--- pacmak.py
+++ pacmak.py
@@ -1,27 +1,38 @@
-import sys, subprocess
+import sys, subprocess#, os
 
 printmode = False
 syncmode = False
 artixmode = False
+
+#os.getenv()
+
 for i in sys.argv[1]:
+    if i == "S":
+        syncmode = True
     if i == "p":
         printmode = True
-    elif i == "S":
-        syncmode = True
 
 if printmode:
-    stdout = subprocess.run(["pacman"]+sys.argv[1:], capture_output=True).stdout
-    print(stdout)
+    pacman_command = ["pacman"]+sys.argv[1:]
+    print(subprocess.run(pacman_command, capture_output=True).stdout)
 
 elif syncmode:
-    stdout = subprocess.run(["pacman"]+sys.argv[1:]+["--print", "--print-format", "%n", "--noprogressbar"], check=True, capture_output=True).stdout
+    getpackages_command = ["pacman"] + sys.argv[1:] + ["--print", "--print-format", "%n", "--noprogressbar", "--noconfirm"]
+    stdout = subprocess.run(getpackages_command, capture_output=True).stdout
     packages = stdout.split()
     
     if artixmode:
-        print("")
-    else:
-        subprocess.run(["asp", "export"]+packages, check=True, capture_output=True).stdout
         for pack in packages:
-            subprocess.run(["cd", pack, "&&", "makepkg", "-sci"], check=True, capture_output=True).stdout
-            print("Installed", pack)
+            git_command = ["git", "clone", "https://github.com/artixlinux/main/"+pack+".git"]
+            print(subprocess.run(git_command, capture_output=True).stdout)
+    else:
+        asp_command = ["asp", "export"] + packages
+        print(subprocess.run(asp_command, capture_output=True).stdout)
+    
+    for pack in packages:
+        make_command = ["cd", pack, "&&", "makepkg", "-sci"]
+        print(subprocess.run(make_command, capture_output=True).stdout)
 
+else:
+    pacman_command = ["pacman"]+sys.argv[1:]
+    print(subprocess.run(pacman_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout)
