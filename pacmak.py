478ef2692008768b765c8f9dabd2cffc3d0abe35
Sakari Marttinen <sakari.marttinen@paivola.fi>
2022-11-07 12:40:01 +0200

    Finish alpha version of sync

--- pacmak.py
+++ pacmak.py
@@ -1,6 +1,8 @@
 import sys, subprocess
 
 printmode = False
+syncmode = False
+artixmode = False
 for i in sys.argv[1]:
     if i == "p":
         printmode = True
@@ -12,6 +14,14 @@ if printmode:
     print(stdout)
 
 elif syncmode:
-    subprocess.run(["pacman"]+sys.argv[1:]+["--print", "--print-format", "%n", "--noprogressbar"], check=True, capture_output=True).stdout
+    stdout = subprocess.run(["pacman"]+sys.argv[1:]+["--print", "--print-format", "%n", "--noprogressbar"], check=True, capture_output=True).stdout
     packages = stdout.split()
+    
+    if artixmode:
+        print("")
+    else:
+        subprocess.run(["asp", "export"]+packages, check=True, capture_output=True).stdout
+        for pack in packages:
+            subprocess.run(["cd", pack, "&&", "makepkg", "-sci"], check=True, capture_output=True).stdout
+            print("Installed", pack)
 
